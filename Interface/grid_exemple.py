0558afc21e0057603fab7e56545ed8a2590b2a66
Ennaox <alexis.laplanche@hotmail.fr>
2021-11-28 00:51:12 +0100

    Ajout code python d'exemple redimention dynamique de widget

---
+++ Interface/grid_exemple.py
@@ -0,0 +1,11 @@
+from tkinter import * 
+
+fenetre = Tk()
+label = Label(fenetre, text="Hello World")
+for ligne in range(5):
+    for colonne in range(5):
+        Grid.rowconfigure(fenetre,ligne, weight=1)
+        Grid.columnconfigure(fenetre,colonne, weight=1)
+        Button(fenetre, text='L%s-C%s' % (ligne, colonne), borderwidth=1).grid(row=ligne, column=colonne, sticky="nsew")
+
+fenetre.mainloop()
