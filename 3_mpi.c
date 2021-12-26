9576a72ba8d7ae14a6e424243a104343a815d7bd
sbstndbs <sebastiendubois4@gmail.com>
2021-12-27 00:27:03 +0100

    nbody in MPI process
    - the whole computation is in the rank ==0 .
    -future : multiprocessing the acceleration loop

--- 3_mpi.c
+++ 3_mpi.c
@@ -11,8 +11,9 @@
 #include <stdlib.h>
 #include <immintrin.h>
 #include <unistd.h>
+#include <mpi.h>
 
-#define SAVE
+//#define SAVE
 
 //
 typedef float              f32;
@@ -118,97 +119,47 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 		rzi = _mm256_loadu_ps(&z[i]);			
 		for (u64 j = 8; j < n + 8; j+=8)
 		{
-		//printf("j ; %lld\n", j);		
-			//printf("j : %lld\n", j);
-			
+
 			rxj = _mm256_load_ps(&x[j]);	
 			ryj = _mm256_load_ps(&y[j]);
 			rzj = _mm256_load_ps(&z[j]);
-			
-
-
 			rdx = _mm256_sub_ps(rxj, rxi) ; 
 			rdy = _mm256_sub_ps(ryj, ryi) ; 
 			rdz = _mm256_sub_ps(rzj, rzi) ;
-
-
-			 
 			rxj = _mm256_mul_ps(rdx, rdx) ;
 			rzj = _mm256_mul_ps(rdz, rdz) ;
-			
-
-						
-			
 			rdxyz = _mm256_add_ps(rxj, rzj) ;
 			ryj =   _mm256_mul_ps(rdy, rdy) ;
-			rdxyz = _mm256_add_ps(rdxyz, ryj) ;			
-			//rdxyz = _mm256_mul_ps(ryj, rdxyz) ;	
+			rdxyz = _mm256_add_ps(rdxyz, ryj) ;				
 			rdxyz = _mm256_add_ps(rsoft, rdxyz) ;	
-			//rxj = _mm256_sqrt_ps(rdxyz) ;
-			//rdxyz = _mm256_mul_ps(rxj, rxj) ;	
-			//rdxyz = _mm256_mul_ps(rdxyz, rxj) ;	
-			
-			//rdx = _mm256_div_ps(rdx, rdxyz) ; 			
-			//rdy = _mm256_div_ps(rdy, rdxyz) ; 
-			//rdz = _mm256_div_ps(rdz, rdxyz) ; 			
-			
-			rxj = _mm256_rsqrt_ps(rdxyz);
-		
-			
+			rxj = _mm256_rsqrt_ps(rdxyz);	
 			rdxyz = _mm256_mul_ps(rxj, rxj) ;	
-			rdxyz = _mm256_mul_ps(rdxyz, rxj) ;
-						
+			rdxyz = _mm256_mul_ps(rdxyz, rxj) ;					
 			rdx = _mm256_mul_ps(rdx, rdxyz) ; 			
 			rdy = _mm256_mul_ps(rdy, rdxyz) ; 
 			rdz = _mm256_mul_ps(rdz, rdxyz) ; 
-		/*	
-		printf("valeur test0 : %f\n", rdx[0]) ;	
-		printf("valeur test1 : %f\n", rdx[1]) ;
-		printf("valeur test2 : %f\n", rdx[2]) ;	
-		printf("valeur test3 : %f\n", rdx[3]) ;
-		printf("valeur test4 : %f\n", rdx[4]) ;	
-		printf("valeur test5 : %f\n", rdx[5]) ;				
-		printf("valeur test6 : %f\n", rdx[6]) ;							
-		printf("valeur test7 : %f\n", rdx[7]) ;	
-		printf("---------------\n");								
-		*/	
 			rfx = _mm256_add_ps(rfx, rdx) ; 	
 			rfy = _mm256_add_ps(rfy, rdy) ; 	
 			rfz = _mm256_add_ps(rfz, rdz) ; 
-			
 							
 		}
-		
-			//sleep(1) ;	
+			
 		rfx = _mm256_mul_ps(rfx, rt) ; 	
 		rfy = _mm256_mul_ps(rfy, rt) ; 	
-		rfz = _mm256_mul_ps(rfz, rt) ; 
- 				
-		
+		rfz = _mm256_mul_ps(rfz, rt) ; 		
 		rvxi = _mm256_loadu_ps(&vx[i]);
 		rvyi = _mm256_loadu_ps(&vy[i]);
 		rvzi = _mm256_loadu_ps(&vz[i]);
-		
-	
-		
 		rvxi = _mm256_add_ps(rvxi, rfx) ; 	
 		rvyi = _mm256_add_ps(rvyi, rfy) ; 
-		rvzi = _mm256_add_ps(rvzi, rfz) ; 
-		
-		
-		
+		rvzi = _mm256_add_ps(rvzi, rfz) ; 	
 		_mm256_storeu_ps(&vx[i], rvxi) ;				
 		_mm256_storeu_ps(&vy[i], rvyi) ;	
-		_mm256_storeu_ps(&vz[i], rvzi) ;
-		
-		
+		_mm256_storeu_ps(&vz[i], rvzi) ;	
 		rfx = _mm256_setzero_ps() ;	
 		rfy = _mm256_setzero_ps() ;	
 		rfz = _mm256_setzero_ps() ;
-		
-
-
-		
+			
 	}
 	
 	
@@ -243,7 +194,14 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 //
 int main(int argc, char **argv)
 {
-	//
+	// MPI init 
+	
+	int rank , size ; 
+	MPI_Init(&argc, &argv) ; 
+	MPI_Comm_rank(MPI_COMM_WORLD , &rank) ; 
+	MPI_Comm_size(MPI_COMM_WORLD , &size) ; 
+	
+	
 	const u64 n = (argc > 1) ? atoll(argv[1]) : 16384;
 	const u64 steps= 10;
 	const f32 dt_value = 0.01;
@@ -269,26 +227,22 @@ int main(int argc, char **argv)
 	const u64 warmup = 3;
 
 	//
+	if (rank == 0){
+		float *x = aligned_alloc(64, sizeof(float) * (n+128)) ; 
+		float *y = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
+		float *z = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
+		float *vx = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
+		float *vy = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
+		float *vz = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
+		float *softening = aligned_alloc(64, sizeof(float) * 64) ; 
+		float *dt = aligned_alloc(64, sizeof(float) * 64) ; 	
+		init(x, y,z, vx, vy, vz, softening, softening_value, dt, dt_value,  n);
+		const u64 sx = 6 *  sizeof(double) * n;
+		printf("\n\033[1mTotal memory size:\033[0m %llu B, %llu KiB, %llu MiB\n\n", sx, sx >> 10, sx >> 20);
+		//
+		printf("\033[1m%5s %10s %10s %8s\033[0m\n", "Step", "Time, s", "Interact/s", "GFLOP/s"); fflush(stdout);
+	
 
-	float *x = aligned_alloc(64, sizeof(float) * (n+128)) ; 
-	float *y = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
-	float *z = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
-	float *vx = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
-	float *vy = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
-	float *vz = aligned_alloc(64, sizeof(float) * (n+128)) ; 	
-	float *softening = aligned_alloc(64, sizeof(float) * 64) ; 
-	float *dt = aligned_alloc(64, sizeof(float) * 64) ; 	
-	//
-	init(x, y,z, vx, vy, vz, softening, softening_value, dt, dt_value,  n);
-
-	const u64 sx = 6 *  sizeof(double) * n;
-
-	printf("\n\033[1mTotal memory size:\033[0m %llu B, %llu KiB, %llu MiB\n\n", sx, sx >> 10, sx >> 20);
-
-	//
-	printf("\033[1m%5s %10s %10s %8s\033[0m\n", "Step", "Time, s", "Interact/s", "GFLOP/s"); fflush(stdout);
-
-	//
 	for (u64 i = 0; i < steps; i++)
 	{
 	
@@ -364,7 +318,8 @@ int main(int argc, char **argv)
 		fclose(xfilePtr); 
 		fclose(vfilePtr) ; 
 	#endif	
-
+	}
+	MPI_Finalize() ; 
 	//
 	return 0;
 }
