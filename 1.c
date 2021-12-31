c087a9f9f5472be0fd0f5f42960ad95ba1386222
sbstndbs <sebastien.dubois4@gmail.com>
2021-12-31 05:41:09 +0100

    Correction of bug
    - the a computation is now effective. The corresponding lines aren;'t suppressed by compilator now.
    - now each process return computed value (fast). The mesured time is :
    	- send array to each process
    	- compute a
    	- return computed a value to the main process (rank 0)
    - there is a huge difference between timings (mote than 2 factor). This is weird. Many
    	verificatuions have to be made

--- 1.c
+++ 1.c
@@ -14,28 +14,56 @@ int main(int argc, char** argv)
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	
+	unsigned long long size_array = 1000000 ; //1000000
+	double * array = malloc( size_array * sizeof(double)) ; 
+	
 
-	int number;
+	
+	double a = 0.0 ; 
+	unsigned long long i_max =  2000000 ;	
+	
 	if (world_rank == 0) {
 		printf("The number of processes is %d\n" , world_size) ;
+		
+		// init array
+		for (unsigned long long i = 0 ; i < size_array ; i++){
+			array[i] = 2.0 ; 
+		}
+		
 		// timer
 		struct timespec start , end ; 
-		clock_gettime( CLOCK_REALTIME, &start ) ; 
+		clock_gettime( CLOCK_MONOTONIC, &start ) ; 
 			
-	for (int i = 1 ; i < world_size ; i++){
-		number = i;
-		MPI_Send(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD);
-	}
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Send(array, size_array, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
+		}
+
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Recv(array+i, 1, MPI_DOUBLE, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
 	
-	// time spend in Send execution    	
-	clock_gettime( CLOCK_REALTIME, &end ) ; 
-	printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;
+
+		printf("value computed : %f \n" , array[1]) ;	
+	
+		// time spend in Send execution    	
+		clock_gettime( CLOCK_MONOTONIC, &end ) ; 
+		printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;
 	    	
 	} else if (world_rank < world_size ) {
-		MPI_Recv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
+		MPI_Recv(array, size_array, MPI_DOUBLE, 0, world_rank, MPI_COMM_WORLD,
 			MPI_STATUS_IGNORE);
-		printf("Process %d received value %d from process 0\n",
-			world_rank , number);
+	
+		// computation \approx same time than recv
+		for (unsigned long long i = 0 ; i < i_max ; i++){
+			a = a + 1.0 ; 
+		}				
+		//printf("Process %d received value %d from process 0\n", world_rank , number);
+	
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Send(&a, 1, MPI_DOUBLE, 0, i, MPI_COMM_WORLD);
+		}	
+	
+
 	}	
 	MPI_Finalize();
 }
--- 1_I.c
+++ 1_I.c
@@ -17,34 +17,65 @@ int main(int argc, char** argv)
 	MPI_Status special_status ;	
 	MPI_Request special_request = MPI_REQUEST_NULL ;
 			
-
-	int number;
+	unsigned long long size_array = 1000000 ; //1000000
+	double * array = malloc( size_array * sizeof(double)) ; 
+	
+	double a = 0.0 ; 
+	unsigned long long i_max = 2000000;
+	
 	if (world_rank == 0) {
 		printf("The number of processes is %d\n" , world_size) ;
+		
+		// init array
+		for (unsigned long long i = 0 ; i < size_array ; i++){
+			array[i] = 2.0 ; 
+		}		
+		
 		// timer	
 		struct timespec start , end ; 
-		clock_gettime( CLOCK_REALTIME, &start ) ; 		
+		clock_gettime( CLOCK_MONOTONIC, &start ) ; 		
 			
 		for (int i = 1 ; i < world_size ; i++){
-			number = i;
-		    	MPI_Isend(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD, &special_request);
+		    	MPI_Isend(array, size_array, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &special_request);
 	    	}
 	    
 		for (int i = 1 ; i < world_size ; i++){
-			number = i;
 			MPI_Wait(&special_request , &special_status);
 		}
-			    
-	// time spend in Send execution  	    	
-	clock_gettime( CLOCK_REALTIME, &end ) ; 
-	printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;	    	
+	
+	
+		for (int i = 1 ; i < world_size ; i++){
+		    	MPI_Irecv(array+i, 1, MPI_DOUBLE, i, i, MPI_COMM_WORLD , &special_request);
+	    	}
+	    
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Wait(&special_request , &special_status); 
+		}	
+		
+		printf("value computed : %f \n" , array[1]) ;
+						    
+		// time spend in Send execution  	    	
+		clock_gettime( CLOCK_MONOTONIC, &end ) ; 
+		printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;	    	
 	    	
 	} else if (world_rank < world_size ) {	
-		MPI_Irecv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
+		MPI_Irecv(array, size_array, MPI_DOUBLE, 0, world_rank, MPI_COMM_WORLD,
 			&special_request);
+
+		// computation \approx same time than recv			
+		for (unsigned long long i = 0 ; i < i_max ; i++){
+			a = a + 1.0 ; 
+		}	
+			
 		MPI_Wait(&special_request , &special_status ) ; 
-		printf("Process %d received value %d from process 0\n",
-			world_rank , number);
+		
+		for (int i = 1 ; i < world_size ; i++){
+			MPI_Isend(&a, 1, MPI_DOUBLE, 0, i, MPI_COMM_WORLD, &special_request) ;
+			MPI_Wait(&special_request , &special_status) ; 
+		}
+		
+		//printf("Process %d received value %d from process 0\n", world_rank , number);
+ 		
 	}	
 	MPI_Finalize();
 }
