b368f0a26146a9e3a93cc54b279a6b6599f59134
sbstndbs <sebastien.dubois4@gmail.com>
2021-12-31 04:40:10 +0100

    Clean and add comments
    - Clean indentation and spaces
    - Add basic comments

--- 1.c
+++ 1.c
@@ -9,35 +9,33 @@
 int main(int argc, char** argv)
 {
 	MPI_Init(&argc,&argv);
-	
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	
- 
-	
 
 	int number;
 	if (world_rank == 0) {
 		printf("The number of processes is %d\n" , world_size) ;
-		
-	struct timespec start , end ; 
-	clock_gettime( CLOCK_REALTIME, &start ) ; 
+		// timer
+		struct timespec start , end ; 
+		clock_gettime( CLOCK_REALTIME, &start ) ; 
 			
-	    for (int i = 1 ; i < world_size ; i++){
-	    	    number = i;
-	    	MPI_Send(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD);
-	    	}
-	    	
+	for (int i = 1 ; i < world_size ; i++){
+		number = i;
+		MPI_Send(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+	}
+	
+	// time spend in Send execution    	
 	clock_gettime( CLOCK_REALTIME, &end ) ; 
-	printf("test time is  %ld\n" , (end.tv_nsec - start.tv_nsec)) ;
+	printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;
 	    	
 	} else if (world_rank < world_size ) {
-	    MPI_Recv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
-		     MPI_STATUS_IGNORE);
-	    printf("Process %d received value %d from process 0\n",
-		   world_rank , number);
+		MPI_Recv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
+			MPI_STATUS_IGNORE);
+		printf("Process %d received value %d from process 0\n",
+			world_rank , number);
 	}	
 	MPI_Finalize();
 }
--- 1_I.c
+++ 1_I.c
@@ -20,35 +20,31 @@ int main(int argc, char** argv)
 
 	int number;
 	if (world_rank == 0) {
-		
-
 		printf("The number of processes is %d\n" , world_size) ;
-		//MPI_Request* requests = malloc( world_rank * sizeof(MPI_Request)) ; 
-
-		//MPI_Status* statuss = malloc( world_rank * sizeof(MPI_Status)) ; 
-		
-	struct timespec start , end ; 
-	clock_gettime( CLOCK_REALTIME, &start ) ; 		
+		// timer	
+		struct timespec start , end ; 
+		clock_gettime( CLOCK_REALTIME, &start ) ; 		
 			
-	    for (int i = 1 ; i < world_size ; i++){
-	    	    number = i;
-	    	MPI_Isend(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD, &special_request);
-	    }
+		for (int i = 1 ; i < world_size ; i++){
+			number = i;
+		    	MPI_Isend(&number, 1, MPI_INT, i, i, MPI_COMM_WORLD, &special_request);
+	    	}
 	    
-	    for (int i = 1 ; i < world_size ; i++){
-	    	    number = i;
-	    	MPI_Wait(&special_request , &special_status);
-	    }	    
-	    	
+		for (int i = 1 ; i < world_size ; i++){
+			number = i;
+			MPI_Wait(&special_request , &special_status);
+		}
+			    
+	// time spend in Send execution  	    	
 	clock_gettime( CLOCK_REALTIME, &end ) ; 
-	printf("test time is  %ld\n" , (end.tv_nsec - start.tv_nsec)) ;	    	
+	printf("Spend %ld in MPI_Send\n" , (end.tv_nsec - start.tv_nsec)) ;	    	
 	    	
 	} else if (world_rank < world_size ) {	
-	    MPI_Irecv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
-		     &special_request);
+		MPI_Irecv(&number, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD,
+			&special_request);
 		MPI_Wait(&special_request , &special_status ) ; 
-	    printf("Process %d received value %d from process 0\n",
-		   world_rank , number);
+		printf("Process %d received value %d from process 0\n",
+			world_rank , number);
 	}	
 	MPI_Finalize();
 }
