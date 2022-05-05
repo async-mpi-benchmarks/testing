3e2c7420fbda9e57b053977495bf22929a75cf97
lamia.dendani <lamia.dendani@ens.uvsq.fr>
2022-05-05 14:33:37 +0200

    Ajout dun test avec les collectives Ibcast et Ireduce

--- test_barrier.c
+++ test_barrier.c
@@ -4,18 +4,23 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc,&argv);
 
-    int rank;
+    int rank , P;
+    MPI_Comm_size(MPI_COMM_WORLD,&P);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     int buff=3;
     if(rank==0)
     {
         MPI_Send(&buff,1,MPI_INT,1,123,MPI_COMM_WORLD);
+        printf("processor %d  sent %d\n",rank,buff);
+        //simulating computation 
+        nanosleep()
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Recv(&buff,1,MPI_INT,1,123,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     }
     else
     {
         MPI_Recv(&buff,1,MPI_INT,0,123,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        printf("processor %d  sent %d\n",rank,buff);
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Send(&buff,1,MPI_INT,0,123,MPI_COMM_WORLD);
     }
